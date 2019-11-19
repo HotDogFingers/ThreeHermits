aecb25ab0fd3d1ce6cc2b9b4524874004d1fffc6
Mission Bit <missionbit@26a-MissionBit.local>
2019-11-18 17:55:11 -0800

    updated js

--- js/game.js
+++ js/game.js
@@ -1,39 +1,37 @@
 
-/* Game namespace */
+/* game namespace */
 var game = {
-
-    // an object where to store game information
+    /**
+     * an object where to store game global data
+     */
     data : {
-        // score
-        score : 0
+      score : 0
     },
-
-
+  
     // Run on page load.
-    "onload" : function () {
-        // Initialize the video.
-        if (!me.video.init(960, 640, {wrapper : "screen", scale : "auto"})) {
-            alert("Your browser does not support HTML5 canvas.");
-            return;
-        }
-
-        // Initialize the audio.
-        me.audio.init("mp3,ogg");
-
-        // set and load all resources.
-        // (this will also automatically switch to the loading screen)
-        me.loader.preload(game.resources, this.loaded.bind(this));
+    onload : function () {
+      // Initialize the video.
+      if (!me.video.init(640, 480, {wrapper : "screen", scale : "auto", scaleMethod : "flex-width"})) {
+        alert("Your browser does not support HTML5 canvas.");
+        return;
+      }
+  
+      // Initialize the audio.
+      me.audio.init("mp3,ogg");
+  
+      // set all ressources to be loaded
+      me.loader.preload(game.resources, this.loaded.bind(this));
     },
-
+  
     // Run on game resources loaded.
-    "loaded" : function () {
-        me.state.set(me.state.MENU, new game.TitleScreen());
-        me.state.set(me.state.PLAY, new game.PlayScreen());
-
-        // add our player entity in the entity pool
-        me.pool.register("mainPlayer", game.PlayerEntity);
-
-        // Start the game.
-        me.state.change(me.state.PLAY);
+    loaded : function () {
+      me.state.set(me.state.MENU, new game.TitleScreen());
+      me.state.set(me.state.PLAY, new game.PlayScreen());
+  
+      // add our player entity in the entity pool
+      me.pool.register("mainPlayer", game.PlayerEntity);
+  
+      // Start the game.
+      me.state.change(me.state.PLAY);
     }
-};
+  };
