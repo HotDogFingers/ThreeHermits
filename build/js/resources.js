729877cf2e0cc7a02471a26b178fb429dba57ace
Mission Bit <missionbit@26a-MissionBit.local>
2019-12-02 16:34:00 -0800

    game2

---
+++ build/js/resources.js
@@ -0,0 +1,192 @@
+game.resources = [
+    {
+        "name": "dst-inertexponent",
+        "type": "audio",
+        "src": "data/bgm/"
+    },
+    {
+        "name": "cling",
+        "type": "audio",
+        "src": "data/sfx/"
+    },
+    {
+        "name": "jump",
+        "type": "audio",
+        "src": "data/sfx/"
+    },
+    {
+        "name": "stomp",
+        "type": "audio",
+        "src": "data/sfx/"
+    },
+    {
+        "name": "area01_bkg0",
+        "type": "image",
+        "src": "data/img/area01_bkg0.png"
+    },
+    {
+        "name": "area01_bkg1",
+        "type": "image",
+        "src": "data/img/area01_bkg1.png"
+    },
+    {
+        "name": "title_screen",
+        "type": "image",
+        "src": "data/img/gui/title_screen.png"
+    },
+    {
+        "name": "Adventurer Sprite Sheet v1.3",
+        "type": "image",
+        "src": "data/img/map/Adventurer Sprite Sheet v1.3.png"
+    },
+    {
+        "name": "area01_level_tiles",
+        "type": "image",
+        "src": "data/img/map/area01_level_tiles.png"
+    },
+    {
+        "name": "Cobra Sprite Sheet",
+        "type": "image",
+        "src": "data/img/map/Cobra Sprite Sheet.png"
+    },
+    {
+        "name": "Destructible Objects Sprite Sheet",
+        "type": "image",
+        "src": "data/img/map/Destructible Objects Sprite Sheet.png"
+    },
+    {
+        "name": "Layer_0000_9",
+        "type": "image",
+        "src": "data/img/map/Layer_0000_9.png"
+    },
+    {
+        "name": "Layer_0001_8",
+        "type": "image",
+        "src": "data/img/map/Layer_0001_8.png"
+    },
+    {
+        "name": "Layer_0002_7",
+        "type": "image",
+        "src": "data/img/map/Layer_0002_7.png"
+    },
+    {
+        "name": "Layer_0003_6",
+        "type": "image",
+        "src": "data/img/map/Layer_0003_6.png"
+    },
+    {
+        "name": "Layer_0004_Lights",
+        "type": "image",
+        "src": "data/img/map/Layer_0004_Lights.png"
+    },
+    {
+        "name": "Layer_0005_5",
+        "type": "image",
+        "src": "data/img/map/Layer_0005_5.png"
+    },
+    {
+        "name": "Layer_0006_4",
+        "type": "image",
+        "src": "data/img/map/Layer_0006_4.png"
+    },
+    {
+        "name": "Layer_0007_Lights",
+        "type": "image",
+        "src": "data/img/map/Layer_0007_Lights.png"
+    },
+    {
+        "name": "Layer_0008_3",
+        "type": "image",
+        "src": "data/img/map/Layer_0008_3.png"
+    },
+    {
+        "name": "Layer_0009_2",
+        "type": "image",
+        "src": "data/img/map/Layer_0009_2.png"
+    },
+    {
+        "name": "Layer_0010_1",
+        "type": "image",
+        "src": "data/img/map/Layer_0010_1.png"
+    },
+    {
+        "name": "Mimic Sprite Sheet",
+        "type": "image",
+        "src": "data/img/map/Mimic Sprite Sheet.png"
+    },
+    {
+        "name": "Spider Tile Set",
+        "type": "image",
+        "src": "data/img/map/Spider Tile Set.png"
+    },
+    {
+        "name": "gripe_run_right",
+        "type": "image",
+        "src": "data/img/sprite/gripe_run_right.png"
+    },
+    {
+        "name": "spinning_coin_gold",
+        "type": "image",
+        "src": "data/img/sprite/spinning_coin_gold.png"
+    },
+    {
+        "name": "wheelie_right",
+        "type": "image",
+        "src": "data/img/sprite/wheelie_right.png"
+    },
+    {
+        "name": "game",
+        "type": "tmx",
+        "src": "data/map/game.tmx"
+    },
+    {
+        "name": "game2",
+        "type": "tmx",
+        "src": "data/map/game2.tmx"
+    },
+    {
+        "name": "Gamelevel1",
+        "type": "tmx",
+        "src": "data/map/Gamelevel1.tmx"
+    },
+    {
+        "name": "Adventurer",
+        "type": "tsx",
+        "src": "data/map/Adventurer.tsx"
+    },
+    {
+        "name": "Decoration Tiles",
+        "type": "tsx",
+        "src": "data/map/Decoration Tiles.tsx"
+    },
+    {
+        "name": "Destroy",
+        "type": "tsx",
+        "src": "data/map/Destroy.tsx"
+    },
+    {
+        "name": "Ground Tiles",
+        "type": "tsx",
+        "src": "data/map/Ground Tiles.tsx"
+    },
+    {
+        "name": "Mimic",
+        "type": "tsx",
+        "src": "data/map/Mimic.tsx"
+    },
+    {
+        "name": "Platformer",
+        "type": "tsx",
+        "src": "data/map/Platformer.tsx"
+    },
+    {
+        "name": "Snake",
+        "type": "tsx",
+        "src": "data/map/Snake.tsx"
+    },
+    {
+        "name": "Spider",
+        "type": "tsx",
+        "src": "data/map/Spider.tsx"
+    }
+];
--- js/screens/play.js
+++ js/screens/play.js
@@ -4,7 +4,7 @@ game.PlayScreen = me.ScreenObject.extend({
      */
     onResetEvent : function () {
       // load a level
-      me.levelDirector.loadLevel("Game");
+      me.levelDirector.loadLevel("game2");
   
       // reset the score
       game.data.score = 0;
